433794cc035ea8b673dbdd08e5b4d1fcf883f3f1
Minelloe <1041321@N138-BF6491D112.KF.local>
2019-12-19 07:57:38 +0100

    second commit

--- Fotoalbum/index.php
+++ Fotoalbum/index.php
@@ -7,18 +7,16 @@ Fotoablum
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title></title>
-        
+
         <!-- CSS -->
         <link rel="stylesheet" href="CSS/css_fotoalbum.css">
-        
-        <!-- FONTS -->
-        
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
-              integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-        
+        <!-- FONTS -->
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
         <!-- Favicons created with realfavicongenerator.net -->
         <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
@@ -29,18 +27,49 @@ Fotoablum
         <meta name="msapplication-TileColor" content="#2d89ef">
         <meta name="msapplication-config" content="favicons/browserconfig.xml">
         <meta name="theme-color" content="#ffffff">
-        
+
     </head>
     <body>
-        <h1>Fotoalbum</h1>
-        <?php
-        // put your code here
-        ?>
+
+        <div class="container">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner container"  >
+                    <div class="carousel-item active">
+                        <img src="https://i.pinimg.com/474x/01/98/64/0198646c0eb76219eb684d482e9963cc.jpg" class="d-block w-100" width="10px" height="500px" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://i.pinimg.com/originals/81/c8/fe/81c8fea5e757251f64ab6a5d954ce546.jpg" class="d-block w-100" width="250px" height="500px" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://i.pinimg.com/originals/81/c8/fe/81c8fea5e757251f64ab6a5d954ce546.jpg" class="d-block w-100" width="250px" height="500px" alt="...">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <h1>Fotoalbum</h1>
+
+        </div>
     </body>
-    
+
     <!-- Optional JavaScript -->
-    
+
     <!-- Custom File first, then jQuery, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
     <script src="js/myscripts.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
             integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
